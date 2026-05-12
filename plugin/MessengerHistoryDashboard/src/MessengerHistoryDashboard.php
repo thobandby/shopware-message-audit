4eeb35cd5d907e196363742aa6dadbbbc854e7e1
thorsten <baumann.it.dienstleistungen@gmail.com>
2026-05-12 12:36:03 +0200

    Refactor plugin sources to resolve Sonar findings

--- plugin/MessengerHistoryDashboard/src/MessengerHistoryDashboard.php
+++ plugin/MessengerHistoryDashboard/src/MessengerHistoryDashboard.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 namespace MessengerHistoryDashboard;
 
 use Shopware\Core\Framework\Plugin;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class MessengerHistoryDashboard extends Plugin
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $this->buildDefaultConfig($container);
+    }
 }
